b0808a898237bcbdf8be395b0f3263449721f133
mithila1422 <77951095+mithila1422@users.noreply.github.com>
2021-06-14 01:12:37 +0600

    Update Lab_03(02)_php.php

--- Lab_03_php/Lab_03(02)_php.php
+++ Lab_03_php/Lab_03(02)_php.php
@@ -1,17 +1,6 @@
 <?php
+	$amount    = 1000;
+	echo "The amount is =$amount<br/>";
+	echo "VAT = " .(10/100 * $amount)."%";
 
-//The VAT rate / percentage.
-$vat = 0.15;
-
-//The price, excluding VAT.
-$priceExcludingVat = 15;
-
-//Calculate how much VAT needs to be paid.
-$vatToPay = ($priceExcludingVat / 100) * $vat;
-
-//The total price, including VAT.
-$totalPrice = $priceExcludingVat + $vatToPay;
-
-//Print out the final price, with VAT added.
-//Format it to two decimal places with number_format.
-echo number_format($totalPrice, 2);
+?>
